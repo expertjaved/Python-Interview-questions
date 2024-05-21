0b5052f049948b7c5ae7d60bcd68c1742c7ec8b7
expertjaved <expertjaved@gmail.com>
2024-05-21 12:30:10 +0530

    added one more logic in uniqueelemts.py file

---
+++ additionalQuestions/uniqueelement.py
@@ -0,0 +1,16 @@
+#list =[1,2,3,1,2,4] . find unique list for example [1234]
+
+
+my_list = [1, 2, 3, 1, 2, 4]
+unique_list = []
+for elem in my_list:
+    if elem not in unique_list:
+        unique_list.append(elem)
+
+print(unique_list)
+
+#we can program in another way as well by using set() function
+#my_list = [1, 2, 3, 1, 2, 4]
+#unique_list = list(set(my_list))
+
+#print(unique_list)
